d2504230701ee9e56e79c302b46c85ec676a4798
sergeymarchuk <sm@marich.pro>
2020-12-08 09:08:01 +0200

    added filterable trait

---
+++ app/Models/Filters/QueryFilter.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Models\Filters;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+abstract class QueryFilter {
+
+    /** @var Request  */
+    protected $request;
+
+    /** @var Builder */
+    protected $builder;
+
+    /**
+     * QueryFilter constructor.
+     * @param Request $request
+     */
+    public function __construct(Request $request) {
+        $this->request = $request;
+    }
+
+    /**
+     * @param Builder $builder
+     */
+    public function apply(Builder $builder): void {
+        $this->builder = $builder;
+
+        foreach ($this->fields() as $field => $value) {
+            $method = Str::camel($field);
+
+            if (method_exists($this, $method)) {
+                call_user_func_array([$this, $method], (array)$value);
+            }
+        }
+    }
+
+    /**
+     * @return array
+     */
+    protected function fields(): array {
+        return array_filter(
+            array_map('trim', $this->request->all())
+        );
+    }
+
+    /**
+     * @param string $value
+     */
+    protected function sort(string $value): void {
+        collect(explode(',', $value))->mapWithKeys(function (string $field) {
+            switch (substr($field, 0, 1)) {
+                case '-':
+                    return [substr($field, 1) => 'desc'];
+                case ' ':
+                    return [substr($field, 1) => 'asc'];
+                default:
+                    return [$field => 'asc'];
+            }
+        })->each(function (string $order, string $field) {
+            $this->builder->orderBy($field, $order);
+        });
+    }
+}
